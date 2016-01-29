cac21e6275a757f7961bfe2ab3bf2cad4024cdfb
Andreas Gillström <andreasgillstrom@gmail.com>
2016-01-29 07:17:00 +0100

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,10 +1,7 @@
-'use strict';
-var test = require('ava');
-var mphToKmh = require('./');
+import test from 'ava';
+import m from './';
 
-test('mphToKmh', function (t) {
-	t.plan(2);
-
-	t.assert(mphToKmh(1) === 1.609344);
-	t.assert(mphToKmh(50) === 80.4672);
+test('mphToKmh', t => {
+	t.is(m(1), 1.609344);
+	t.is(m(50), 80.4672);
 });
